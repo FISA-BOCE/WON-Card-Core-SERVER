cdce59548deecc608cf40e84b0dba9134e01f9b6
Chae-Yu <chaeyu2004@naver.com>
2026-05-15 12:11:45 +0900

    Setting: Spring Boot 초기 세팅 설정

---
+++ src/main/java/com/woorifisa/won_card_core_server/WonCardCoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_card_core_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonCardCoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonCardCoreServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woorifisa/won_card_core_server/WonCardCoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_card_core_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonCardCoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

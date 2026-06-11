fcebea0f464a0cdf0835df0c2ee43faf36871b72
chaeyu <chaeyu2004@naver.com>
2026-06-11 15:36:36 +0900

    Fix: sweep_request_id unique constraint 추가

--- src/main/java/com/woorifisa/won_card_core_server/domain/reward/model/CardPointLedger.java
+++ src/main/java/com/woorifisa/won_card_core_server/domain/reward/model/CardPointLedger.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -25,7 +26,15 @@ import org.hibernate.type.SqlTypes;
 
 @Entity
 @Getter
-@Table(name = "card_point_ledger")
+@Table(
+        name = "card_point_ledger",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_card_point_ledger_sweep_request_id",
+                        columnNames = "sweep_request_id"
+                )
+        }
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CardPointLedger extends BaseTimeEntity {
 
